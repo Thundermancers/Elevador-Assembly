af52ee1ae7030ad68567e3e1d510ec93622f76ea
Lucas Peixoto de Almeida Cavalcante <lpdac@ic.ufal.br>
2018-09-04 10:01:37 -0300

    Ajustando o log.

--- log.py
+++ log.py
@@ -1,83 +1,103 @@
 import serial, time
+import datetime
 import numpy as np
 
-SERIAL_PORT = '/dev/ttyACM0'
-SERIAL_RATE = 115200
 
-def main():
-	ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
-	mode = 1
-	print('Modo programador? Y\\n')
-	answer = input()
-	if answer == 'n':
-		mode = 0
-	
-	while True:
-		reading = ser.readline()
-		reading = str(reading)
-		arr = reading.split("_")[1:-1]
-		out = []
-		
-		if not len(arr) == 7:
-			continue
-		else:
-			for s in arr:
-				s = s.split(' ')
-				if not len(s) == 2: 
-					continue				
-				out.append( [s[0], s[1]] )
+class Log :
+    def __init__(self, SERIAL_PORT, SERIAL_RATE) :
+        self.now = None
+        self.mapping = {}
+        self.mapping[0] = 'térreo'
+        self.mapping[1] = '1° andar'
+        self.mapping[2] = '2° andar'
+        self.mapping[3] = '3° andar'
+        self.init_serial(SERIAL_PORT, SERIAL_RATE)
 
-		if not len(out) == 7:
-			continue
+    def getCurrentDateTime(self) :
+        self.now = datetime.datetime.now()
 
-		if mode :
-			for s in out:
-				if len(s[1]) == 1:
-					print(s[0] , ord(s[1])-ord('0'))
-				else :
-					print(s[0] , s[1] )
-		else:
+    def init_serial(self, SERIAL_PORT, SERIAL_RATE) :
+        self.ser = serial.Serial(SERIAL_PORT, SERIAL_RATE)
 
-			# 1000 - STOP_ELE
-			# 0000 - STOP
-			# 0101 - DES_ELE
-			# 0111 - RISE_ELE
-			# 0001 - DES_OUT
-			# 0011 - RISE_OUT
+    def run(self) :
+        mode = 1
+        print('Modo programador? S\\n')
+        answer = input()
+        if answer == 'n':
+            mode = 0
 
-			'''
-				E 0000
-				OS 0000
-				L 0
-				O 0
-				S 0000
-				DC :
-				FC 0
-			'''			
+        while True:
+            self.getCurrentDateTime()
+            reading = self.ser.readline()
+            reading = str(reading)
+            arr = reading.split("_")[1:-1]
+            out = []
 
-			if int(out[4][1],2)&1 :
-				print( 'Parado', end='')
+            if not len(arr) == 7:
+                continue
+            else:
+                for s in arr:
+                    s = s.split(' ')
+                    if not len(s) == 2: 
+                        continue				
+                    out.append( [s[0], s[1]] )
 
-			else :
-				if int(out[4][1],2)&2 :
-					print('Subindo para ', end='')
-					print( int(np.log2(int(out[4][1],2))), end='' )
-				else:
-					print('Descendo para ', end='')
-					v = int(out[4][1],2)
-					l = int(out[2][1])
-					bitmask = (1<<l) - 1
-					v = v&bitmask
-					print( int(np.log2(v)), end='' )				
+            if not len(out) == 7:
+                continue
 
-				print(' pela chamada do ')
-				if int(out[0][1],2):
-					print('elevador')
-				else
-					print('andar')
-			
-			pass
-		
+            if mode :
+                for s in out:
+                    if len(s[1]) == 1:
+                        print(s[0] , ord(s[1])-ord('0'))
+                    else :
+                        print(s[0] , s[1] )
+            else:
+
+                # 1000 - STOP_ELE
+                # 0000 - STOP
+                # 0101 - DES_ELE
+                # 0111 - RISE_ELE
+                # 0001 - DES_OUT
+                # 0011 - RISE_OUT
+
+                '''
+                    E 0000
+                    OS 0000
+                    L 0
+                    O 0
+                    S 0000
+                    DC :
+                    FC 0
+                '''			
+
+                print(self.now.strftime('%Y-%m-%d %H:%M:%S'), '- ', end='')
+                if not int(out[4][1],2)&1 :
+                    print('Parado no' , self.mapping[int(out[2][1])], end='; ')
+
+                else :
+                    if int(out[4][1],2)&2 :
+                        print('Subindo para o ', end='')
+                        print( self.mapping[int(out[2][1]) + 1], end ='')
+                    else:
+                        print('Descendo para o ', end='')
+                        print( self.mapping[int(out[2][1]) - 1], end='')				
+
+                    print(' pela chamada do ', end='')
+                    if int(out[4][1],2)&4:
+                        print('elevador', end ='; ')
+                    else :
+                        print('andar', end='; ')
+
+                if int(out[3][1]) == 2 :
+                    print('Porta aberta')
+                elif int(out[3][1]) == 3  :
+                    print('Porta aberta e buzzer ligado')
+                else :
+                    print('Porta fechada')
+
 
 if __name__ == "__main__":
-	main()
+    port = '/dev/ttyACM0'
+    rate = 115200
+    log = Log(port, rate)
+    log.run()
